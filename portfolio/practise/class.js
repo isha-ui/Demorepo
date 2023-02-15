e4e52744d5ec329a8a537e6cfeec371abdc5f6d7
isha-ui <ishavaghera2821@gmail.com>
2023-02-15 16:38:29 +0530

    demo commit

---
+++ portfolio/practise/class.js
@@ -0,0 +1,9 @@
+class name{
+    constructor(fname,lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
+
+var n = new name("Isha","Vaghera");
+document.write(n.fname+" "+n.lname);
